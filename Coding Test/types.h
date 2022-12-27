ae06e85cfc179eee5f3ab09fdd1c1761a8ae1fc2
Zach-Suyoung <68154270+Jack-Suyoung@users.noreply.github.com>
2022-12-28 08:10:13 +0900

    Add types.h

---
+++ Coding Test/types.h
@@ -0,0 +1,12 @@
+#define PI 3.14159265358791
+#define DEG2RAD (PI/180.0)
+#define RAD2DEG (180.0/PI)
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef float float32_t;
+typedef double float64_t;
