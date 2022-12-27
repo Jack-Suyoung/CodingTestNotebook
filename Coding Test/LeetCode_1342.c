a8b7de7fbae42e31006a492c80a593e8f9676a11
Zach-Suyoung <68154270+Jack-Suyoung@users.noreply.github.com>
2022-12-28 08:06:22 +0900

    Add LeetCode 1342 Number of Steps to Reduce a Number to Zero

---
+++ Coding Test/LeetCode_1342.c
@@ -0,0 +1,26 @@
+
+int main()
+{
+    return 0;
+}
+
+int numberOfSteps(int num) {
+
+    int step = 0;
+
+    while (num != 0)
+    {
+        if (num % 2 == 0)
+        {
+            num = num / 2;
+        }
+        else
+        {
+            num -= 1;
+        }
+
+        ++step;
+    }
+
+    return step;
+}
