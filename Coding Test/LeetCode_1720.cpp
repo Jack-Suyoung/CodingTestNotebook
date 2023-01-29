2deb252497911f819e6ae06e89076b62db375d4b
Zach-Suyoung <68154270+Jack-Suyoung@users.noreply.github.com>
2023-01-29 20:03:18 +0900

    Add LeetCode 1720

---
+++ Coding Test/LeetCode_1720.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+using namespace std;
+
+// 1720. Decode XORRed Array
+
+//There is a hidden integer array arr that consists of n non - negative integers.
+//
+//It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1].
+//For example, if arr = [1, 0, 2, 1], then encoded = [1, 2, 3].
+//
+//You are given the encoded array.You are also given an integer first, that is the first element of arr, i.e.arr[0].
+//
+//Return the original array arr.It can be proved that the answer existsand is unique.
+
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> res;
+
+        res.push_back(first);
+
+        for (int i = 0; i != encoded.size(); ++i)
+        {
+            res.push_back(res.back() ^ encoded[i]);
+        }
+
+        return res;
+    }
+};
